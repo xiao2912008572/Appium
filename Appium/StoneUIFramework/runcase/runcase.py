60897492d15347e4c9c017020f2ddaa7440b356b
xiao291008572 <291008572@qq.com>
2017-09-20 11:05:52 +0800

    20170920

--- Appium/StoneUIFramework/runcase/runcase.py
+++ Appium/StoneUIFramework/runcase/runcase.py
@@ -3,7 +3,9 @@ import unittest
 import time
 
 import sys
-sys.path.append("C:\\Users\\xiaoj\PycharmProjects\Appium")
+# sys.path.append("C:\\Users\\xiaoj\PycharmProjects\Appium")
+sys.path.append("C:\Program Files (x86)\Jenkins\workspace\jenkins_github_test1\Appium")
+
 # sys.path.append('/usr/local/StoneUIFramework')
 # sys.path.append("/usr/local/Appium")
 
