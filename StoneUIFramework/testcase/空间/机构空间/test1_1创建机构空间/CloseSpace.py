a666e06a1db96368bf14165f766779512cef5726
xiaojingyuan <291008572@qq.com>
2017-10-05 22:00:22 +0800

    20170930 commit 更新pycharm格式

--- StoneUIFramework/testcase/空间/机构空间/test1_1创建机构空间/CloseSpace.py
+++ StoneUIFramework/testcase/空间/机构空间/test1_1创建机构空间/CloseSpace.py
@@ -6,40 +6,41 @@ from StoneUIFramework.public.handle.space.SPACEHANDLE5 import _SPACEHANDLE5
 from StoneUIFramework.public.common.datainfo import DataInfo
 from StoneUIFramework.public.common.log import Log
 
-#关闭机构空间
+
+# 关闭机构空间
 class CloseSpace:
-    def closeSpace(self,driver):
-        #创建_SPACEHANDLE5公有定位控件对象
+    def closeSpace(self, driver):
+        # 创建_SPACEHANDLE5公有定位控件对象
         handle = _SPACEHANDLE5(driver)
-        #测试数据
-        d = DataInfo("space.xls")#创建DataInfo()对象
+        # 测试数据
+        d = DataInfo("space.xls")  # 创建DataInfo()对象
         # self.easyname = d.cell("test001",2,2,)
-        self.menu_x = int(d.cell("test001-创建",2,11))#fullname
-        self.menu_y = int(d.cell("test001-创建",2,12,))#easyname
-        #创建读取配置信息对象
-        cf = GlobalParam('config','path_file.conf')
-        #获取截图路径、日志路径、日志名
-        self.logfile = cf.getParam('log',"logfile")#日志文件名
-        #创建日志模块
+        self.menu_x = int(d.cell("test001-创建", 2, 11))  # fullname
+        self.menu_y = int(d.cell("test001-创建", 2, 12, ))  # easyname
+        # 创建读取配置信息对象
+        cf = GlobalParam('config', 'path_file.conf')
+        # 获取截图路径、日志路径、日志名
+        self.logfile = cf.getParam('log', "logfile")  # 日志文件名
+        # 创建日志模块
         self.log = Log(self.logfile)
         sleep(1)
         try:
             self.log.info("------START:test1_1创建机构空间.CloseSpace.py-----")
-            #为了保证不中途退出，需要第一次进入的时候检查是否存在该机构，如果存在，先关闭
-            handle.Kjlb_browseorgspaceByID_click(0)#点击进入
+            # 为了保证不中途退出，需要第一次进入的时候检查是否存在该机构，如果存在，先关闭
+            handle.Kjlb_browseorgspaceByID_click(0)  # 点击进入
             self.log.info('点击进入该空间')
             sleep(1)
-            handle.Kjlb_browseorgspace_menu_click()#菜单栏
+            handle.Kjlb_browseorgspace_menu_click()  # 菜单栏
             self.log.info('点击空间菜单栏')
-            handle.Kjlb_browseorgspace_menu_bcard_click()#企业名片
+            handle.Kjlb_browseorgspace_menu_bcard_click()  # 企业名片
             self.log.info('点击企业名片')
-            handle.Kjlb_browseorgspace_menu_bcard_menu_click()#菜单栏
+            handle.Kjlb_browseorgspace_menu_bcard_menu_click()  # 菜单栏
             self.log.info('点击菜单栏')
-            handle.Kjlb_browseorgspace_menu_bcard_menu_close_click()#关闭
+            handle.Kjlb_browseorgspace_menu_bcard_menu_close_click()  # 关闭
             self.log.info('点击关闭按钮')
-            handle.Kjlb_browseorgspace_menu_bcard_menu_close_confirm_click()#确认关闭
+            handle.Kjlb_browseorgspace_menu_bcard_menu_close_confirm_click()  # 确认关闭
             self.log.info('点击确认关闭')
             self.log.info("------END:test1_1创建机构空间.CloseSpace.pyy-----")
         except Exception as err:
-            self.log.error("CloseSpace Inside : %s"%err)
+            self.log.error("CloseSpace Inside : %s" % err)
             raise err
--- StoneUIFramework/testcase/空间/机构空间/test1_1创建机构空间/CreateOrgSpace001_1.py
+++ StoneUIFramework/testcase/空间/机构空间/test1_1创建机构空间/CreateOrgSpace001_1.py
@@ -12,63 +12,65 @@ from StoneUIFramework.public.handle.space.SPACEHANDLE5 import _SPACEHANDLE5
 from StoneUIFramework.public.common.datainfo import DataInfo
 from StoneUIFramework.public.common.log import Log
 
-#创建机构空间
+
+# 创建机构空间
 class space_CreateO(unittest.TestCase):
-    @classmethod#装饰器，类方法
-    def setUpClass(self):#最开始执行
-        #建立连接信息
+    @classmethod  # 装饰器，类方法
+    def setUpClass(self):  # 最开始执行
+        # 建立连接信息
         cnn = Connect()
         self.driver = cnn.connect()
-        #创建工具类
-        self.tools = Tools(self.driver)#tools工具
-        #创建_BrowseOrgSpace公有定位控件对象
+        # 创建工具类
+        self.tools = Tools(self.driver)  # tools工具
+        # 创建_BrowseOrgSpace公有定位控件对象
         self.handle = _SPACEHANDLE5(self.driver)
-        #创建读取配置信息对象
-        cf = GlobalParam('config','path_file.conf')
-        #获取截图路径、日志路径、日志名
-        self.screen_path = cf.getParam('space',"org_path_001_1")#通过配置文件获取截图的路径
-        self.logfile = cf.getParam('log',"logfile")#日志文件名
-        #创建日志记录模块
+        # 创建读取配置信息对象
+        cf = GlobalParam('config', 'path_file.conf')
+        # 获取截图路径、日志路径、日志名
+        self.screen_path = cf.getParam('space', "org_path_001_1")  # 通过配置文件获取截图的路径
+        self.logfile = cf.getParam('log', "logfile")  # 日志文件名
+        # 创建日志记录模块
         self.log = Log(self.logfile)
-        #创建Createspace和Closespace对象
+        # 创建Createspace和Closespace对象
         self.cr = CreateSpace()
         self.cl = CloseSpace()
         sleep(1)
-        #测试数据
-        d = DataInfo("space.xls")#创建DataInfo()对象
-        self.fullname = d.cell("test001-创建",2,1)#fullname
-        self.easyname = d.cell("test001-创建",2,2,)#easyname
+        # 测试数据
+        d = DataInfo("space.xls")  # 创建DataInfo()对象
+        self.fullname = d.cell("test001-创建", 2, 1)  # fullname
+        self.easyname = d.cell("test001-创建", 2, 2, )  # easyname
+
     def test_spacecreate(self):
         '''创建机构空间'''
         try:
             self.log.info("------------START:test1_1创建机构空间.CreateOrgSpace001_1.py------------")
-            #-------------创建机构空间------------
-            #先进行判断，空间是否存在，如果不存在，创建；如果存在，先删除后创建
+            # -------------创建机构空间------------
+            # 先进行判断，空间是否存在，如果不存在，创建；如果存在，先删除后创建
             sleep(1)
-            self.handle.Kjlb_click()#进入空间列表
+            self.handle.Kjlb_click()  # 进入空间列表
             self.log.info('进入空间列表')
-            #遍历空间列表，查找是否存在该空间
+            # 遍历空间列表，查找是否存在该空间
             self.flag = 0
             for element in self.handle.Kjlb_browseorgspace_getElements():
                 spacename = element.text
                 if spacename == self.easyname:
-                    self.flag =1
+                    self.flag = 1
                     self.log.info('已存在该空间')
                     break
                 else:
                     pass
-            #如果机构存在
+            # 如果机构存在
             if self.flag == 1:
-                self.cl.closeSpace(self.driver)#关闭
-                self.cr.createSpace(self.driver,self.fullname,self.easyname)#关闭之后,重新创建机构空间
+                self.cl.closeSpace(self.driver)  # 关闭
+                self.cr.createSpace(self.driver, self.fullname, self.easyname)  # 关闭之后,重新创建机构空间
                 self.cl.closeSpace(self.driver)
             else:
-                self.cr.createSpace(self.driver,self.fullname,self.easyname)#创建机构空间
+                self.cr.createSpace(self.driver, self.fullname, self.easyname)  # 创建机构空间
                 self.cl.closeSpace(self.driver)
-            self.log.info("------------END:test1_1创建机构空间.CreateOrgSpace001_1.py------------")#宣布成功
+            self.log.info("------------END:test1_1创建机构空间.CreateOrgSpace001_1.py------------")  # 宣布成功
         except Exception as err:
-            self.tools.getScreenShot(self.screen_path,"ExceptionShot")
-            self.log.error("Outside : %s"%err)
+            self.tools.getScreenShot(self.screen_path, "ExceptionShot")
+            self.log.error("Outside : %s" % err)
             raise err
         finally:
             self.driver.quit()
