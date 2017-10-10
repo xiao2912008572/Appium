0dccd0255dce16e071f86f3af5565f22556a7e36
xiaojingyuan <291008572@qq.com>
2017-10-10 17:39:37 +0800

    commit 20171010 update ddt数据驱动模块 机构空间test1_1,test2_1已加

--- StoneUIFramework/testcase/空间/机构空间/test3_1团队人事任免/TeamAssignJob.py
+++ StoneUIFramework/testcase/空间/机构空间/test3_1团队人事任免/TeamAssignJob.py
@@ -1,38 +1,25 @@
 __author__ = 'Administrator'
 # -*- coding: utf-8 -*-
 from time import sleep
-from StoneUIFramework.public.common.publicfunction import Tools
 from StoneUIFramework.public.handle.space.SPACEHANDLE5 import _SPACEHANDLE5
-from StoneUIFramework.public.common.datainfo import DataInfo
 from StoneUIFramework.public.common.log import Log
 from StoneUIFramework.config.globalparam import GlobalParam
 
 
 # 团队人事任免
 class TeamAssignJob:
-    def __init__(self):  # 初始化测试数据
-        d = DataInfo("space.xls")  # 创建DataInfo()对象
-        self.spacename = d.cell("test003-团队", 2, 1)  # 测试空间123
-        self.AdministratorLoc = int(d.cell("test003-团队", 2, 2))  # 管理员编辑
-        self.SalespersonLoc = int(d.cell("test003-团队", 2, 2))  # 销售员编辑
-        self.AssistantLoc = int(d.cell("test003-团队", 2, 4))  # 行政助理编辑
-        self.AdmNum = int(d.cell("test003-团队", 2, 5))  # 管理员人数
-        self.SalNum = int(d.cell("test003-团队", 2, 6))  # 销售员人数
-        self.AssNum = int(d.cell("test003-团队", 2, 7))  # 行政助理人数
-        self.Name = d.cell("test003-团队", 2, 8)  # 肖静远
-        self.Director = int(d.cell("test003-团队", 2, 9))  # 董事会
-        self.Marketing = int(d.cell("test003-团队", 2, 10))  # 营销部
-        self.HR = int(d.cell("test003-团队", 2, 11))  # 人事部
-        # 创建读取配置信息对象
+    #1. 初始化
+    def __init__(self):
+        # 1.创建读取配置信息对象
         cf = GlobalParam('config', 'path_file.conf')
-        # 获取截图路径、日志路径、日志名
+        # 2.获取截图路径、日志路径、日志名
         self.logfile = cf.getParam('log', "logfile")  # 日志文件名
-        # 创建日志模块
+        # 3.创建日志模块
         self.log = Log(self.logfile)
 
-    def teamAssignJob(self, driver):
-        # 创建工具类
-        tools = Tools(driver)  # tools工具
+    def teamAssignJob(self, driver, AdminstratorLoc,
+                      SalespersonLoc, AssistanLoc, AdmNum, SalNum,
+                      AssNum, Name, Director, Marketing, Hr):
         # 创建_OrgSpaceTeamHandle公有定位控件对象
         handle = _SPACEHANDLE5(driver)
         sleep(1)
@@ -57,8 +44,8 @@ class TeamAssignJob:
             self.log.info('编辑管理员人数')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_claer()  # 清空
             self.log.info('清空输入框')
-            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(self.AdmNum)  # 2人
-            self.log.info('设置管理员人数：%s' % self.AdmNum)
+            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(AdmNum)  # 2人
+            self.log.info('设置管理员人数：%s' % AdmNum)
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_confirm_click()  # 点击是
             self.log.info('点击是')
             # 4.2 销售员人数:3人
@@ -67,8 +54,8 @@ class TeamAssignJob:
             self.log.info('编辑销售员人数')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_claer()  # 清空
             self.log.info('清空输入框')
-            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(self.SalNum)  # 3人
-            self.log.info('设置销售员人数：%s' % self.SalNum)
+            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(SalNum)  # 3人
+            self.log.info('设置销售员人数：%s' % SalNum)
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_confirm_click()  # 点击是
             self.log.info('点击是')
             # 4.3 行政助理人数:4人
@@ -77,8 +64,8 @@ class TeamAssignJob:
             self.log.info('编辑助理人数')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_claer()  # 清空
             self.log.info('清空输入框')
-            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(self.AssNum)  # 4人
-            self.log.info('设置助理人数：%s' % self.AssNum)
+            handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_sendkeys(AssNum)  # 4人
+            self.log.info('设置助理人数：%s' % AssNum)
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_confirm_click()  # 点击是
             self.log.info('点击是')
             # 5.检查各职位人数是否保存生效
@@ -88,8 +75,8 @@ class TeamAssignJob:
             self.log.info('点击编辑管理员人数')
             AdmNumm = int(handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_text())
             self.log.info('当前管理员人数：{0}'.format(AdmNumm))
-            self.log.info('预期管理员人数：{0}'.format(self.AdmNum))
-            assert self.AdmNum == AdmNumm, "编辑管理员人数保存后未生效"
+            self.log.info('预期管理员人数：{0}'.format(AdmNum))
+            assert AdmNum == AdmNumm, "编辑管理员人数保存后未生效"
             self.log.info('检查管理员人数编辑是否生效')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_cancel_click()  # 点击否
             self.log.info('点击否')
@@ -97,10 +84,10 @@ class TeamAssignJob:
             # handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_click(self.SalespersonLoc)#编辑管理员人数
             driver.find_elements_by_id("com.yunlu6.stone:id/companyteam_item_edit")[1].click()
             self.log.info('点击编辑销售员人数')
-            SalNum = int(handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_text())
-            self.log.info('当前销售员人数：{0}'.format(SalNum))
-            self.log.info('预期销售员人数：{0}'.format(self.SalNum))
-            assert self.SalNum == SalNum, "编辑销售员人数保存后未生效"
+            SalNumm = int(handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_text())
+            self.log.info('当前销售员人数：{0}'.format(SalNumm))
+            self.log.info('预期销售员人数：{0}'.format(SalNum))
+            assert SalNum == SalNumm, "编辑销售员人数保存后未生效"
             self.log.info('检查销售人员人数编辑是否生效')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_cancel_click()  # 点击否
             self.log.info('点击否')
@@ -108,10 +95,10 @@ class TeamAssignJob:
             # handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_click(self.AssistantLoc)#编辑管理员人数
             driver.find_elements_by_id("com.yunlu6.stone:id/companyteam_item_edit")[3].click()
             self.log.info('点击行政助理人数')
-            AssNum = int(handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_text())
-            self.log.info('当前行政助理人数：{0}'.format(AssNum))
-            self.log.info('预期行政助理人数：{0}'.format(self.AssNum))
-            assert self.AssNum == AssNum, "编辑行政助理人数后未生效"
+            AssNumm = int(handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_jobsnumedit_text())
+            self.log.info('当前行政助理人数：{0}'.format(AssNumm))
+            self.log.info('预期行政助理人数：{0}'.format(AssNum))
+            assert AssNum == AssNumm, "编辑行政助理人数后未生效"
             self.log.info('检查行政助理人数编辑是否生效')
             handle.Kjlb_browseorgspace_menu_team_teamedit_numeidt_cancel_click()  # 点击否
             self.log.info('点击否')
@@ -127,7 +114,7 @@ class TeamAssignJob:
                 listT = handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact()
                 for i in range(len(listT)):  # 遍历列表
                     # if handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact()[i].text == self.Name:#再判断是否该人已被任免
-                    if handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_text(i) == self.Name:  # 再判断是否该人已被任免
+                    if handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_text(i) == Name:  # 再判断是否该人已被任免
                         handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_click(0)  # 待任免联系人
                         self.log.info('判断该人是否已被任免')
                         self.log.info('点击待任免联系人')
@@ -140,8 +127,8 @@ class TeamAssignJob:
             handle.Kjlb_browseorgspace_menu_team_menu_assignjob_addperson_click()  # 点击人事任免新增按钮
             self.log.info('点击人事任免新增按钮')
             # 8.搜索姓名:肖静远
-            handle.Kjlb_browseorgspace_menu_team_menu_assignjob_addperson_search_sendkeys(self.Name)  # 搜索关键字
-            self.log.info('搜索人脉关键字：%s' % self.Name)
+            handle.Kjlb_browseorgspace_menu_team_menu_assignjob_addperson_search_sendkeys(Name)  # 搜索关键字
+            self.log.info('搜索人脉关键字：%s' % Name)
             handle.Kjlb_browseorgspace_menu_team_menu_assignjob_addperson_searchbtn_click()  # 点击搜索
             self.log.info('点击搜索')
             # 9.点击搜索的结果,添加
@@ -152,7 +139,7 @@ class TeamAssignJob:
             # 10.待任免列表点击联系人-任免职位-勾选-返回
             handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_click(0)  # 待任免联系人
             self.log.info('点击待任免联系人')
-            handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_department_click(self.Director)  # 董事会
+            handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_department_click(Director)  # 董事会
             self.log.info('点击董事会')
             handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_jobname_click(0)  # 董事长
             self.log.info('勾选董事长')
@@ -161,7 +148,7 @@ class TeamAssignJob:
             handle.Kjlb_browseorgspace_menu_team_menu_assignjob_contact_back_click()  # 返回
             self.log.info('返回')
             name = driver.find_elements_by_id("com.yunlu6.stone:id/companyteam_item_name")[0].text  # 获取董事长姓名
-            assert self.Name == name, "董事长任免失败"
+            assert Name == name, "董事长任免失败"
             self.log.info('判断该人脉董事长职位是否任免成功')
             # 11.移除任免,还原
             handle.Kjlb_browseorgspace_menu_team_menu_click()  # 点击菜单栏
