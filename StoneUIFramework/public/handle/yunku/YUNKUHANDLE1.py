2203545aecf759ed9bc7c49d7b208b358c68c89f
xiaojingyuan <291008572@qq.com>
2017-10-13 17:12:17 +0800

    commit 20171011 1.开始重构云库的page和handle公用方法

---
+++ StoneUIFramework/public/handle/yunku/YUNKUHANDLE1.py
@@ -0,0 +1,13 @@
+__author__ = 'xiaoj'
+# 云库首页
+from StoneUIFramework.public.pages.yunku.YUNKUPAGE1 import _YUNKUPAGE1
+
+
+class _SPACEHANDLE1(_YUNKUPAGE1):
+    # ******************************************************【HANDLE1】******************************************************
+    # 云库：点击
+    def Yk_click(self):
+        return self.p.click(self.Yk)
+
+    #云库-添加-相册：点击
+
---
+++ StoneUIFramework/public/pages/yunku/YUNKUPAGE1.py
@@ -0,0 +1,24 @@
+__author__ = 'xiaoj'
+from StoneUIFramework.public.common.basepage import Page
+
+
+class _YUNKUPAGE1(Page):
+    # 定位:云库
+    Yk = ('id->com.yunlu6.stone:id/navi_item_work_cloundlibrary', '云库')
+
+    # 云库-添加
+    Yk_add = ('id->com.yunlu6.stone:id/cloundlibrary_bottom_more', '云库-添加')
+
+    # 云库-添加-相册
+    # 云库-添加-拍照
+    # 云库-添加-wifi同步
+    # 云库-添加-取消
+    # 云库-搜索栏
+    # 云库-搜索按钮
+    # 云库-菜单
+    # 云库-菜单-返回
+    # 云库-菜单-选照片列表
+    # 云库-菜单-选照片列表-分类到
+    # 云库-菜单-选照片列表-删除
+    # 云库-全选
+    # 云库-取消
