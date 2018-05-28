2d01f15e53d67190f7c9f01fdfbacaa58be5c977
xiao291008572 <291008572@qq.com>
2018-05-28 11:29:57 +0800

    test_atuo1

--- YunluFramework_API/runcase/runcase1.py
+++ YunluFramework_API/runcase/runcase1.py
@@ -4,7 +4,8 @@ __author__ = 'xiaoj'
 import time
 import sys
 sys.path.append("/Users/xiaojingyuan/PycharmProjects/Appium")
-sys.path.append("/root/.jenkins/workspace/AutoTest")
+# sys.path.append("/root/.jenkins/workspace/AutoTest")
+sys.path.append("/root/.jenkins/workspace")
 
 # sys.path.append("C:\Program Files (x86)\Jenkins\workspace\jenkins_github_test1\\")
 # sys.path.append("\\Users\\xiaojingyuan\.jenkins\workspace\stest\\")
