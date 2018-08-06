eabc55684fa024faa44270def6ab25389aca1edd
xiao291008572 <291008572@qq.com>
2018-08-06 13:56:53 +0800

    xjy

--- YunluFramework_API/testcase/SpaceAPI/TestSpaceApi.py
+++ YunluFramework_API/testcase/SpaceAPI/TestSpaceApi.py
@@ -61,7 +61,7 @@ class SpaceAPI_Private(unittest.TestCase):
     # data = excel1.get_row_data(sheet_name='topics')
 
     '''
-    20180712 - lin 
+    20180712 - 肖静远 
     '''
     request = API_REQUEST(sheet_name='topics')
     excel1 = Excel(xls='data_api.xls', sheet_name='topics')
